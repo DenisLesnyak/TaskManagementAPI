f6e8e3e6cadc5d23b3a738d6f4fea2a745528de1
Денис <105715080+DenisLesnyak@users.noreply.github.com>
2025-01-17 17:14:04 +0300

    Update TaskModel.cs
    Добавил комменты TaskModel.cs

--- TaskManagementAPI/Models/TaskModel.cs
+++ TaskManagementAPI/Models/TaskModel.cs
@@ -1,7 +1,7 @@
-﻿public class TaskModel
+public class TaskModel
 {
-    public int Id { get; set; } // Уникальный идентификатор задачи
-    public string Title { get; set; } // Название задачи
-    public string Status { get; set; } // Статус задачи (например, "В процессе", "Завершена")
-    public DateTime Deadline { get; set; } // Крайний срок
+    public int Id { get; set; } // Уникальный идентификатор задачи, используется для различия задач в базе данных
+    public string Title { get; set; } // Название задачи, которое описывает суть задачи
+    public string Status { get; set; } // Текущий статус задачи, например, "Новая", "В процессе", "Завершена"
+    public DateTime Deadline { get; set; } // Крайний срок выполнения задачи
 }
